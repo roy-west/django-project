e269b2e234896b06e0ac53276fa31e112e349e1f
asylbektestgithub <asylbek1100@gmail.com>
2022-08-30 20:46:57 +0600

    new updates

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+""""
+# one to one relationship
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+# one to many relationship
+class Capital(models.Model):
+    country = models.OneToOneField(Country,
+                                   on_delete=models.CASCADE,
+                                   primary_key=True)
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Subject(models.Model):
+    teachers = models.ManyToManyField(Teacher)
+    title = models.CharField(max_length=250)
+"""
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+import random
+
 
 # Create your views here.
+
+def main_page(request):
+    return HttpResponse("Hello, World !")
+
+
+def test_view(request):
+    print(request)
+    my_object = Post.objects.all()
+    len_of_objects = len(my_object)
+    my_random_object_from_db = Post.objects.get(id=random.randint(1, len_of_objects))
+    list_ids = [1, 4, 2]
+    all_items_from_db = Post.objects.filter(id__in=list_ids)
+    context = {"my_first_object": my_random_object_from_db,
+               "my_list": all_items_from_db}
+
+    return render(request, 'posts/test.html', context=context)
