c6cc8c5810d4211f6e186b7396e52e2c6f982515
asylbektestgithub <asylbek1100@gmail.com>
2022-09-03 19:05:22 +0600

    new updated

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
 
-admin.site.register(Post)
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'timestamp', 'publish']
+
+
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    publish = models.DateField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
